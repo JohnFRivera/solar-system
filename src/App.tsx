314e8f448d4408b4516d199ed3299c36375b6b06
JohnFRivera <johnfrivera1309@gmail.com>
2026-01-13 11:43:53 -0500

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SimulationCanvas } from '@/components/SimulationCanvas';
+
+/**
+ * Application
+ */
+export default function App() {
+  return (
+    <>
+      <SimulationCanvas />
+    </>
+  );
+}
